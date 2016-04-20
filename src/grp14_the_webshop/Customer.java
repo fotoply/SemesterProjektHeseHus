6cbf88ed4f00c03ddb3a13dc34126113c96a86db
fotoply <norberg.niels@gmail.com>
2016-04-20 14:33:57 +0200

    Added password hashes

--- src/grp14_the_webshop/Customer.java
+++ src/grp14_the_webshop/Customer.java
@@ -3,6 +3,13 @@ package grp14_the_webshop;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
 import java.util.Date;
 import java.util.Random;
 
@@ -13,11 +20,11 @@ import java.util.Random;
 public class Customer {
     String address;
     String email;
-    String password;
+    byte[] password;
     int phoneNumber;
     Date birthday;
     String name;
-    int salt;
+    byte[] salt;
     
     public Customer(String name, String address, String email, String password, Date dayOfBirth, int phoneNumber) {
         this.name = name;
@@ -25,8 +32,9 @@ public class Customer {
         this.email = email;
         this.birthday = dayOfBirth;
         this.phoneNumber = phoneNumber;
-        Random rnd = new Random();
-        this.salt = String.valueOf(rnd.nextFloat()).hashCode();
+        SecureRandom rnd = new SecureRandom();
+        byte[] saltArray = new byte[32];
+        rnd.nextBytes(saltArray);
         setPassword(password);
     }
     
@@ -63,14 +71,25 @@ public class Customer {
     }
 
     public boolean isCorrectPassword(String comparisonPassword) {
-
+        return this.password.equals(getPasswordHash(comparisonPassword));
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password = getPasswordHash(password);
     }
 
-    public String getPasswordHash() {
-
+    public byte[] getPasswordHash(String password) {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        SecretKeyFactory f = null;
+        byte[] hash = new byte[32];
+        try {
+            f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            hash = f.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return hash;
     }
 }
