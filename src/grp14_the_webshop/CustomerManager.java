df5da2e16d3957f4ea7f86724e9d2a1dbde48eae
fotoply <norberg.niels@gmail.com>
2016-04-20 14:44:51 +0200

    Change customerList to a map instead

--- src/grp14_the_webshop/CustomerManager.java
+++ src/grp14_the_webshop/CustomerManager.java
@@ -1,9 +1,8 @@
 
 package grp14_the_webshop;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
@@ -12,22 +11,22 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  */
 public class CustomerManager {
     
-    private List<Customer> customerList;
-    
-    public List getCustomerList() {
-        return this.customerList;
-    }
+    private Map<Integer,Customer> customerMap;
     
     public CustomerManager() {
-        customerList = new ArrayList();
+        customerMap = new HashMap<>();
     }
     
     public void createNewOrder(){
         throw new NotImplementedException();
     }
+
+    private int getNextId() {
+        throw new NotImplementedException();
+    }
     
     public void createCustomer(String name, String address, int phoneNumber, String email, String password, Date dayOfBirth){
-        customerList.add(new Customer(name, address, email, password, dayOfBirth, phoneNumber));
+        customerMap.put(getNextId(),new Customer(name, address, email, password, dayOfBirth, phoneNumber));
         throw new NotImplementedException();
     }
     
@@ -37,7 +36,7 @@ public class CustomerManager {
     }
     
     public void deleteCustomer(int customerID){
-        throw new NotImplementedException();
+
     }
     
     public void addproduct(Product product, int amount, int customerID){
