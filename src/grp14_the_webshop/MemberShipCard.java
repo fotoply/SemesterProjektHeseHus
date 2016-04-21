29f26d8146547c0fb28aa94af7c3d086b17aa936
fotoply <norberg.niels@gmail.com>
2016-04-21 11:18:12 +0200

    Add private modifier

--- src/grp14_the_webshop/MemberShipCard.java
+++ src/grp14_the_webshop/MemberShipCard.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class MemberShipCard {
 
-    int memberShipID;
+    private int memberShipID;
     private List<MemberShipCard> memberShipCard = new ArrayList();
 
     public MemberShipCard(int id) {
