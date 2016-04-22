7c485a41c349d7b98ce12ea9abf9fbbf980ede96
fotoply <norberg.niels@gmail.com>
2016-04-22 10:37:58 +0200

    Tons of getters and a couple of setters

--- src/grp14_the_webshop/Product.java
+++ src/grp14_the_webshop/Product.java
@@ -9,6 +9,8 @@ package grp14_the_webshop;
  * @author Karim
  */
 public class Product {
+    private String name;
+    private String description;
     private String type;
     private Money price;
     private int productID;
@@ -33,5 +35,37 @@ public class Product {
         if (type != null ? !type.equals(product.type) : product.type != null) return false;
         return price != null ? price.equals(product.price) : product.price == null;
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setPrice(Money price) {
+        this.price = price;
+    }
+
+    public boolean isCurrentlySelling() {
+        return currentlySelling;
     }
 }
