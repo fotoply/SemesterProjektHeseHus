a116cc0b3e89aefe978c932422bc674093c149c2
fotoply <norberg.niels@gmail.com>
2016-04-22 10:40:36 +0200

    Add missing constructor to PaymentInStore

--- src/grp14_the_webshop/PaymentInStore.java
+++ src/grp14_the_webshop/PaymentInStore.java
@@ -7,5 +7,9 @@ public class PaymentInStore extends Payment {
     private int reservationID;
     private String location;
 
-
+    public PaymentInStore(Money amount, int reservationID, String location) {
+        super(amount);
+        this.reservationID = reservationID;
+        this.location = location;
+    }
 }
