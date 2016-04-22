01a27b1e0803f1c9d57a512d8319df9f2a40aaab
fotoply <norberg.niels@gmail.com>
2016-04-22 11:10:28 +0200

    Tons of JavaDocs

--- src/webshop/Money.java
+++ src/webshop/Money.java
@@ -11,14 +11,27 @@ public class Money implements Comparable {
 
     private BigDecimal amount;
 
+    /**
+     * Creates a new money object which represents an arbitrary currency.
+     */
     public Money() {
         this("0");
     }
 
+    /**
+     * Creates a new money object which represents an arbitrary currency.
+     * @param amount the starting amount for the new object
+     * @throws IllegalArgumentException If the starting amount is less than 0
+     */
     public Money(String amount) {
         this(new BigDecimal(amount));
     }
 
+    /**
+     * Creates a new money object which represents an arbitrary currency.
+     * @param amount the starting amount for the new object
+     * @throws IllegalArgumentException If the starting amount is less than 0
+     */
     public Money(BigDecimal amount) {
         if (Money.isNegative(amount)) {
             throw new IllegalArgumentException("Money cannot be negative");
@@ -35,22 +48,39 @@ public class Money implements Comparable {
         return amount.compareTo(BigDecimal.ZERO) < 0;
     }
 
+    /**
+     *
+     * @return true if the current money object is negative
+     */
     public boolean isNegative() {
-        return amount.compareTo(BigDecimal.ZERO) < 0;
+        return Money.isNegative(this.amount);
     }
 
     public BigDecimal getAmount() {
         return amount;
     }
 
+    /**
+     *
+     * @return amount as a human readable string
+     */
     public String getAmountAsString() {
         return amount.toPlainString();
     }
 
+    /**
+     * Subtracts the amount in the given money object from the current money object
+     * @param money
+     */
     public void pay(Money money) {
         pay(money.amount.toString());
     }
 
+    /**
+     * Subtracts the amount from the current money object
+     * @param amount
+     * @throws IllegalArgumentException If the amount passed is less than 0
+     */
     public void pay(String amount) {
         if (!isNegative(this.amount.subtract(new BigDecimal(amount)))) {
             this.amount = this.amount.subtract(new BigDecimal(amount));
@@ -59,10 +89,19 @@ public class Money implements Comparable {
         }
     }
 
+    /**
+     * Adds the value of the <code>money</code> object to the current object
+     * @param money
+     */
     public void add(Money money) {
         add(money.amount.toString());
     }
 
+    /**
+     * Adds a specified amount of money to the current object
+     * @param amount
+     * @throws  IllegalArgumentException If the amount is less than 0
+     */
     public void add(String amount) {
         if (isNegative(amount)) {
             throw new IllegalArgumentException("Cannot add negative money, use pay instead");
