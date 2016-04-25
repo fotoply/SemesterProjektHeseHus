168cde2ca3c65d401b789accf95c90709cb7cdbb
fotoply <norberg.niels@gmail.com>
2016-04-25 14:53:40 +0200

    Added createCustomer method

--- src/webshop/WebshopController.java
+++ src/webshop/WebshopController.java
@@ -5,6 +5,10 @@
  */
 package webshop;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -13,18 +17,20 @@ import java.util.Scanner;
 public class WebshopController {
 
     private int memberShipCardID = 0;
+    Webshop webshop = Webshop.getInstance();
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // Test code for products
-        Webshop webshop = Webshop.getInstance();
-        System.out.println("Enter 1 to create a new account. Enter 0 to exit");
+        WebshopController controller = new WebshopController();
+
+        System.out.println("Enter 1 to create a new account.\nEnter 2 to login to existing account.\nEnter 0 to exit");
         Scanner input = new Scanner(System.in);
         switch (input.nextInt()) {
             case 1:
-                createAccount(input);
+                controller.createAccount(input);
                 break;
 
             case 0:
@@ -33,7 +39,23 @@ public class WebshopController {
         }
     }
 
-    private static void createAccount(Scanner input) {
+    private void createAccount(Scanner input) throws ParseException {
+        input.nextLine(); // To remove the previous newline from when the number was entered.
 
+        System.out.println("Enter name:");
+        String name = input.nextLine();
+        System.out.println("Enter address:");
+        String address = input.nextLine();
+        System.out.println("Enter email:");
+        String email = input.nextLine();
+        System.out.println("Enter password:");
+        String password= input.nextLine();
+        System.out.println("Enter birthday (YY-MM-DD):");
+        DateFormat format = new SimpleDateFormat("YY,MM,dd");
+        Date birth = format.parse(input.nextLine());
+        System.out.println("Enter phone number:");
+        int phoneNumber = Integer.valueOf(input.nextLine());
+
+        webshop.createCustomer(name, address,email,password,birth,phoneNumber);
     }
 }
