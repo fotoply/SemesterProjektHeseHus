254092f334ccee3e3517422465573776cd37cb82
Villefrance <VilleV@10.126.42.6>
2016-04-27 12:24:13 +0200

    Product constructor tilføjet + 4 produkter hardcodet ind i ProductCatalog

--- src/webshop/ProductCatelog.java
+++ src/webshop/ProductCatelog.java
@@ -17,6 +17,19 @@ public class ProductCatelog {
 
     public ProductCatelog() {
         productList = new ArrayList<>();
+        
+        productList.add(new Product("Baseball T-Shirt", "Simple and fresh  new Zine 2nd Inning white baseball t-shirt.",
+                "T-Shirt", new Money("599.95"), 0, false));
+        
+        productList.add(new Product("Polo T-Shirt", "A Nice Polo T-shirt",
+                "T-Shirt", new Money("475.95"), 1, true));
+        
+        productList.add(new Product("Black Jeans", "Stylish black rinser colorway with a slight stretch modern straight leg construction for a comfortable fit.",
+                "Jeans", new Money("999.95"), 2, true));
+        
+        productList.add(new Product("Denim Jacket", "Stylish black denim body with an integrated black zip up hoodie and soft fleece lined black knit sleeves.",
+                "Jackets", new Money("1499.95"), 3, true));
+        
     }
 
     public Product findProduct(int itemID) {
