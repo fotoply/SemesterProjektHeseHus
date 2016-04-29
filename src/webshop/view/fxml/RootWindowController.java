c43242912ecbc7cb00b7dfd0e7826a8a9399aed2
fotoply <fotoply@gmail.com>
2016-04-29 17:07:41 +0200

    Added an image to the button to make it look like a menu button

--- src/webshop/view/fxml/RootWindowController.java
+++ src/webshop/view/fxml/RootWindowController.java
@@ -1,11 +1,22 @@
 package webshop.view.fxml;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
 
 public class RootWindowController {
 
     @FXML
     private Button optionsButton;
+
+    @FXML
+    void initialize() {
+        BackgroundImage optionsButtonImage = new BackgroundImage(new Image("/res/menuButton.png"),null,null,null,null);
+        optionsButton.setBackground(new Background(optionsButtonImage));
+        optionsButton.setPadding(Insets.EMPTY);
+    }
 
 }
