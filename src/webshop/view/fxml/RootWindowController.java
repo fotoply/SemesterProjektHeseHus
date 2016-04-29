6ec529fc4e52f8ab490364413016331c29dfa08b
fotoply <fotoply@gmail.com>
2016-04-29 16:58:15 +0200

    Started adding menu and GUI

---
+++ src/webshop/view/fxml/RootWindowController.java
@@ -0,0 +1,11 @@
+package webshop.view.fxml;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class RootWindowController {
+
+    @FXML
+    private Button optionsButton;
+
+}
