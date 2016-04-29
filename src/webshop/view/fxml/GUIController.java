211b08eda3f4df3392f6e210bbef47655a5a4526
fotoply <norberg.niels@gmail.com>
2016-04-29 14:51:01 +0200

    Starting GUI work

---
+++ src/webshop/view/fxml/GUIController.java
@@ -0,0 +1,20 @@
+package webshop.view.fxml;/**
+ * Created 4/29/16
+ *
+ * @author Niels Norberg
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUIController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
