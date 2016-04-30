2a1fba685903190eea5d049ade3f242a6a215226
fotoply <fotoply@gmail.com>
2016-04-30 18:42:08 +0200

    Started work on the login dialog

---
+++ src/webshop/view/fxml/LoginPopupController.java
@@ -0,0 +1,27 @@
+package webshop.view.fxml;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LoginPopupController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void loginPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUpClicked(MouseEvent event) {
+
+    }
+
+}
