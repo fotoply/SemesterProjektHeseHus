4e528af6cc770c57336020c67b90b2c9ab89adae
fotoply <fotoply@gmail.com>
2016-04-30 13:27:45 +0200

    Should now be displayed

--- src/webshop/view/fxml/ProductNode.java
+++ src/webshop/view/fxml/ProductNode.java
@@ -1,6 +1,7 @@
 package webshop.view.fxml;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import webshop.Product;
@@ -17,7 +18,7 @@ public class ProductNode {
     private Text titleTextField;
 
     public void setProduct(Product product) {
-        //productImage.setGraphic
+        productImage.setImage(new Image("/res/placeholderProductIcon"));
         priceTextField.setText(product.getPrice().getAmountAsString());
         titleTextField.setText(product.getName());
     }
