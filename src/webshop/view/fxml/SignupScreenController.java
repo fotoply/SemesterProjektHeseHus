c541e0b1f5216b924ac836db5678c007495e38c3
fotoply <fotoply@gmail.com>
2016-04-30 20:16:02 +0200

    Added logic for creating accounts

--- src/webshop/view/fxml/SignupScreenController.java
+++ src/webshop/view/fxml/SignupScreenController.java
@@ -1,9 +1,14 @@
 package webshop.view.fxml;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import webshop.view.GUIController;
+
+import java.time.Instant;
+import java.util.Date;
 
 public class SignupScreenController {
 
@@ -30,5 +35,23 @@ public class SignupScreenController {
 
     @FXML
     private DatePicker dateOfBirthPicker;
+
+    @FXML
+    void signupPressed(ActionEvent event) {
+        //TODO verify that all data is valid
+        //TODO Find a way to display what data is invalid and why
+
+        if(!emailField.getText().equalsIgnoreCase(repeatEmailField.getText())) {
+            return;
+        }
+
+        if(!passwordField.getText().equalsIgnoreCase(repeatPasswordField.getText())) {
+            return;
+        }
+
+        GUIController.getWebshopInstance().createCustomer(nameField.getText(), addressField.getText(), emailField.getText(), passwordField.getText(), Date.from(Instant.from(dateOfBirthPicker.getValue().atStartOfDay())), Integer.parseInt(phoneField.getText()));
+        GUIController.getRootInstance().showLoginScreen();
+    }
+
 
 }
