cd2ca664f5bb891d1d386b9fdffbe43e58ef913b
fotoply <fotoply@gmail.com>
2016-04-30 20:01:25 +0200

    Add FXId's and add Controller

---
+++ src/webshop/view/fxml/SignupScreenController.java
@@ -0,0 +1,34 @@
+package webshop.view.fxml;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignupScreenController {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private PasswordField repeatPasswordField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField repeatEmailField;
+
+    @FXML
+    private DatePicker dateOfBirthPicker;
+
+}
