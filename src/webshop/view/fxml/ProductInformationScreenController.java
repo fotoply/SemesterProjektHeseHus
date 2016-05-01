00d96e434dc68fa404a37cc7c9b518fbd5f9b00a
fotoply <fotoply@gmail.com>
2016-05-01 23:10:39 +0200

    Add FXId to everything in ProductInformation and add the controller class boilerplate

---
+++ src/webshop/view/fxml/ProductInformationScreenController.java
@@ -0,0 +1,24 @@
+package webshop.view.fxml;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class ProductInformationScreenController {
+
+    @FXML
+    private ImageView productImage;
+
+    @FXML
+    private Text titleText;
+
+    @FXML
+    private TextArea descriptionTextArea;
+
+    @FXML
+    void addToBasketClicked(ActionEvent event) {
+
+    }
+
+}
