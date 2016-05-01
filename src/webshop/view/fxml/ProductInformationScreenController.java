545817a94ad6ed8ff2abb24e5025d6bee2a11056
fotoply <fotoply@gmail.com>
2016-05-01 23:19:02 +0200

    Added textField for entering quantity and made addToBasket add to the basket

--- src/webshop/view/fxml/ProductInformationScreenController.java
+++ src/webshop/view/fxml/ProductInformationScreenController.java
@@ -1,11 +1,16 @@
 package webshop.view.fxml;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import webshop.view.GUIController;
 
 public class ProductInformationScreenController {
+
+    ProductNode product;
 
     @FXML
     private ImageView productImage;
@@ -17,8 +22,11 @@ public class ProductInformationScreenController {
     private TextArea descriptionTextArea;
 
     @FXML
-    void addToBasketClicked(ActionEvent event) {
+    private TextField quantityTextField;
 
+    @FXML
+    void addToBasketClicked(ActionEvent event) {
+        GUIController.getRootInstance().addToBasket(product);
     }
 
 }
