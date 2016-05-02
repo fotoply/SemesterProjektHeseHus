9d9ff8d810293a9f3ca6c3fe50d24d4bb9f49536
fotoply <norberg.niels@gmail.com>
2016-05-02 12:34:09 +0200

    Add some error checking for signup

--- src/webshop/view/fxml/SignupScreenController.java
+++ src/webshop/view/fxml/SignupScreenController.java
@@ -38,6 +38,23 @@ public class SignupScreenController {
     private DatePicker dateOfBirthPicker;
 
     @FXML
+    void initialize() {
+        phoneField.textProperty().addListener((observable, oldValue, newValue) -> {correctPhoneNumber(newValue,oldValue);});
+    }
+
+    private void correctPhoneNumber(String newValue, String oldValue) {
+        if(newValue.isEmpty()) {
+            phoneField.setText(newValue);
+        } else {
+            try {
+                Integer.parseInt(newValue);
+            } catch (NumberFormatException e) {
+                phoneField.setText(oldValue);
+            }
+        } //Add styling for too short phone numbers
+    }
+
+    @FXML
     void signupPressed(ActionEvent event) {
         //TODO verify that all data is valid
         //TODO Find a way to display what data is invalid and why
