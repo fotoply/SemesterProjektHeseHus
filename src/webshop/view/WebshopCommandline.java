94ab5f695cc7f1fe8eada2d93590040331599f9c
Hejle <johej15@student.sdu.dk>
2016-05-02 14:55:58 +0200

    Changed checkoutCustomer - moved checkoutBasket() to the top

--- src/webshop/view/WebshopCommandline.java
+++ src/webshop/view/WebshopCommandline.java
@@ -80,7 +80,8 @@ public class WebshopCommandline {
 
     private boolean checkoutCustomer(Scanner input) {
         input.nextLine();
-
+        webshop.checkoutBasket();
+        
         try {
             if (webshop.getCustomer() == null || webshop.getCurrentOrder() == null) {
                 System.out.println("You cannot checkout without an account or basket");
@@ -127,7 +128,7 @@ public class WebshopCommandline {
 
         System.out.println("Your address is: " + webshop.getCurrentOrder().getShippingAddress());
 
-        webshop.checkoutBasket();
+        
         input.nextLine();
         return true;
     }
