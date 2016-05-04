5a00d8cef6cba21d1bef27f49f82168e95202939
fotoply <norberg.niels@gmail.com>
2016-05-04 14:26:28 +0200

    Added SQL statement and error handling

--- src/webshop/model/Inventory/PostgresDatabaseOrder.java
+++ src/webshop/model/Inventory/PostgresDatabaseOrder.java
@@ -3,6 +3,10 @@ package webshop.model.Inventory;
 import webshop.model.database.DatabaseConnector;
 import webshop.model.database.PostgresConnectionDriver;
 
+import java.sql.ResultSet;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+
 /**
  * Created 5/4/16
  *
@@ -11,9 +15,14 @@ import webshop.model.database.PostgresConnectionDriver;
 public class PostgresDatabaseOrder extends Order {
     DatabaseConnector connector;
 
-    public PostgresDatabaseOrder(PostgresConnectionDriver driver, int orderId) {
+    public PostgresDatabaseOrder(PostgresConnectionDriver driver, int orderId) throws SQLException {
         super(orderId);
         this.connector = driver;
+        ResultSet rs = driver.executeQueryStatement("SELECT * FROM order WHERE orderId=" + orderId);
+        if(rs.next()) {
 
+        } else {
+            throw new SQLDataException("Order not found");
+        }
     }
 }
