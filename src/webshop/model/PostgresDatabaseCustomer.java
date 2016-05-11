d541bad7fc162970e7406a8bfa9d18accd9eff94
fotoply <norberg.niels@gmail.com>
2016-05-11 13:12:15 +0200

    Fix new customerId to be 1 higher

--- src/webshop/model/PostgresDatabaseCustomer.java
+++ src/webshop/model/PostgresDatabaseCustomer.java
@@ -44,7 +44,7 @@ public class PostgresDatabaseCustomer extends Customer {
         int id = -1;
         try {
             ResultSet rs = connectionDriver.executeQuery("SELECT max(customerid) FORM customer");
-            id = rs.getInt("customerid");
+            id = rs.getInt("customerid") + 1;
 
             connectionDriver.executeUpdate(String.format("INSERT INTO customer (customerid, name, address, email, password, birthday, phonenumber, passwordsalt, currentorder) VALUES (%d, '%s', '%s', '%s', '%s', '%s', %d, '%s', %d)", id, name, address, email, Customer.toBase64(getPassword()), dayOfBirth.toInstant(), phoneNumber, Customer.toBase64(getSalt()), -1));
         } catch (SQLException e) {
