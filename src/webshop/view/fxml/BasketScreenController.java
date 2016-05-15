a3ec283c6c124211f819c19a94445453db783bb1
fotoply <fotoply@gmail.com>
2016-05-15 16:13:07 +0200

    Added controller. Still no functionality

---
+++ src/webshop/view/fxml/BasketScreenController.java
@@ -0,0 +1,27 @@
+package webshop.view.fxml;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class BasketScreenController {
+
+    @FXML
+    private TextField giftcodeTextArea;
+
+    @FXML
+    void applyCodeClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void payLaterClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void payNowClicked(ActionEvent event) {
+
+    }
+
+}
