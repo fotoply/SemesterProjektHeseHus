2bcfaf08544fe9f5603e7886a648576289c65b30
fotoply <fotoply@gmail.com>
2016-05-16 13:39:19 +0200

    Added radiobuttons for selected where to deliver the order

--- src/webshop/view/fxml/BasketScreenController.java
+++ src/webshop/view/fxml/BasketScreenController.java
@@ -2,12 +2,23 @@ package webshop.view.fxml;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class BasketScreenController {
 
     @FXML
     private TextField giftcodeTextArea;
+
+    @FXML
+    private RadioButton shopAddressRadio;
+
+    @FXML
+    private ToggleGroup deliveryToggleGroup;
+
+    @FXML
+    private RadioButton ownAddressRadio;
 
     @FXML
     void applyCodeClicked(ActionEvent event) {
