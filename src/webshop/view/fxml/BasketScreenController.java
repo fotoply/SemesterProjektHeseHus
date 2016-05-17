1b01d34a70889bfa7e4d3ead4544c300a28f0e34
fotoply <fotoply@gmail.com>
2016-05-17 22:26:32 +0200

    Added FXML identifier to listview and finished applyBasket

--- src/webshop/view/fxml/BasketScreenController.java
+++ src/webshop/view/fxml/BasketScreenController.java
@@ -2,9 +2,11 @@ package webshop.view.fxml;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
 import webshop.model.Inventory.Item;
 
 import java.math.BigDecimal;
@@ -13,6 +15,7 @@ import java.util.List;
 
 public class BasketScreenController {
 
+    public static final String BASKET_FORMAT_LOCALE = "%s-40 - %s-8 - %s8";
     @FXML
     private TextField giftcodeTextArea;
 
@@ -26,17 +29,22 @@ public class BasketScreenController {
     private RadioButton ownAddressRadio;
 
     @FXML
+    private ListView<Text> basketListView;
+
+    @FXML
     void initialize() {
 
     }
 
     public void applyBasket(List<Item> items) {
-
-
+        basketListView.getItems().add(new Text(String.format(BASKET_FORMAT_LOCALE, "Name", "Quantity", "Total price")));
+        for (Item item: items) {
+            basketListView.getItems().add(new Text(itemToString(item)));
+        }
     }
 
     private String itemToString(Item item) {
-        return String.format("%s-40 - %d-02 - %s-6", item.getProduct().getName(), item.getQuantity(), item.getProduct().getPrice().getAmount().multiply(BigDecimal.valueOf(item.getQuantity())));
+        return String.format(BASKET_FORMAT_LOCALE, item.getProduct().getName(), item.getQuantity(), item.getProduct().getPrice().getAmount().multiply(BigDecimal.valueOf(item.getQuantity())));
     }
 
     @FXML
