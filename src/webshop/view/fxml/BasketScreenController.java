3ada82d49f83c8a2e13616f0d8d615c649897680
fotoply <fotoply@gmail.com>
2016-05-17 22:21:32 +0200

    Added itemToString method

--- src/webshop/view/fxml/BasketScreenController.java
+++ src/webshop/view/fxml/BasketScreenController.java
@@ -5,6 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import webshop.model.Inventory.Item;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BasketScreenController {
 
@@ -19,6 +24,20 @@ public class BasketScreenController {
 
     @FXML
     private RadioButton ownAddressRadio;
+
+    @FXML
+    void initialize() {
+
+    }
+
+    public void applyBasket(List<Item> items) {
+
+
+    }
+
+    private String itemToString(Item item) {
+        return String.format("%s-40 - %d-02 - %s-6", item.getProduct().getName(), item.getQuantity(), item.getProduct().getPrice().getAmount().multiply(BigDecimal.valueOf(item.getQuantity())));
+    }
 
     @FXML
     void applyCodeClicked(ActionEvent event) {
