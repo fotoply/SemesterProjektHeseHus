7bb9ee15cd497b7207a151b18a70293d47ebce9f
Hejle <johej15@student.sdu.dk>
2016-05-18 12:22:15 +0200

    Changed setAmount  to void

--- src/webshop/model/payments/Payment.java
+++ src/webshop/model/payments/Payment.java
@@ -21,7 +21,7 @@ public abstract class Payment {
         return amount;
     }
 
-    public Money setAmount(Money amount) {
+    public void setAmount(Money amount) {
         this.amount = amount;
 
     }
