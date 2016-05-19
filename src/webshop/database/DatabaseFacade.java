b75e0fd55a5060ab167e02bd14f3478c36f09f60
fotoply <norberg.niels@gmail.com>
2016-05-19 12:53:33 +0200

    Add missing apostrofes

--- src/webshop/database/DatabaseFacade.java
+++ src/webshop/database/DatabaseFacade.java
@@ -176,7 +176,7 @@ public class DatabaseFacade implements IDatabaseFacade {
     @Override
     public ResultSet getProductByType(String searchTerms) {
         try {
-            return databaseConnector.executeQuery("SELECT * FROM product WHERE type like " + searchTerms + "%");
+            return databaseConnector.executeQuery("SELECT * FROM product WHERE type like '" + searchTerms + "%'");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -192,7 +192,7 @@ public class DatabaseFacade implements IDatabaseFacade {
     @Override
     public ResultSet getProductByName(String searchTerms) {
         try {
-            return databaseConnector.executeQuery("SELECT * FROM product WHERE name=" + searchTerms + "%");
+            return databaseConnector.executeQuery("SELECT * FROM product WHERE name='" + searchTerms + "%'");
         } catch (SQLException e) {
             e.printStackTrace();
         }
