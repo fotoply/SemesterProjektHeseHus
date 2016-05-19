33c7c1bd84fb916e8f5441219dfad82c1d675f82
fotoply <norberg.niels@gmail.com>
2016-05-19 12:51:40 +0200

    Attempted to make searching functional

--- src/webshop/view/fxml/SearchController.java
+++ src/webshop/view/fxml/SearchController.java
@@ -2,10 +2,20 @@ package webshop.view.fxml;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import webshop.view.GUIController;
 
 public class SearchController {
 
     @FXML
     private TextField searchTextField;
 
+    @FXML
+    void initialize() {
+        searchTextField.textProperty().addListener((observable, oldValue, newValue) -> {searchChanged(newValue);});
+    }
+
+    private void searchChanged(String newValue) {
+        GUIController.getRootInstance().getShopAreaController().loadItems(GUIController.getWebshopInstance().searchProduct(newValue));
+
+    }
 }
